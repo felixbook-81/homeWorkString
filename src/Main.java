2e4390279584e5861e389106faa5e333d26d8912
DiverGent <gamer.felixbook@gmail.com>
2023-01-05 01:21:41 +0300

    commit message

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        // task1();
+        task2();
+        //task3();
+        //task4();
+
+    }
+
+    public static void task1() {
+
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+
+        System.out.println("ФИО сотрудника — " + fullName);
+    }
+
+    public static void task2() {
+
+        String fullName = "Ivanov Ivan Ivanovich";
+        String fullName1=
+        boolean sEqualsS2 = s.equals(s2);
